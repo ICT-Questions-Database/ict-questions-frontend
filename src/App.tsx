90191f47a8f47b42a2646ea9daddc96fae70846b
sara lee <saragatinha@gmail.com>
2025-12-05 11:20:06 -0300

    feat: searchbar element

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { QuestionContainer } from './components/Questions/QuestionContainer'
-import { Questions } from './data/Data'
-import type { Question } from './models/Question'
+import { QuestionContainer } from "./components/Questions/QuestionContainer";
+import { Searchbar } from "./components/Searchbar";
+import { Questions } from "./data/Data";
+import type { Question } from "./models/Question";
 
 function App() {
-  const questions: Question[] = Questions
-  
+  const questions: Question[] = Questions;
+
   return (
     <div>
-      <QuestionContainer question={questions[0]}/>
+      <Searchbar />
+      <QuestionContainer question={questions[0]} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Searchbar.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+export function Searchbar() {
+  const [userQuery, setUserQuery] = useState<string>("")
+  
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setUserQuery(event.target.value)
+  }
+  
+  return (
+    <input
+      className="rounded-md bg-white border border-[#D1D5DB] py-3 px-5 w-full"
+      type="text"
+      placeholder="Busque aqui"
+      value={userQuery}
+      onChange={handleChange}
+    />
+  );
+}
