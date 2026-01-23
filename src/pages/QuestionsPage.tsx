bf27fd6a94a44da9e3845b3cedb2a38b831ceb9a
marcellolee03 <marcellohslee@gmail.com>
2026-01-23 16:35:29 -0300

    fetching questions from API

--- src/pages/QuestionsPage.tsx
+++ src/pages/QuestionsPage.tsx
@@ -1,12 +1,33 @@
+import { useState, useEffect } from "react";
 import { FilterBar } from "../components/FilterBar";
 import { Navbar } from "../components/Navbar";
 import { QuestionContainer } from "../components/Questions/QuestionContainer";
 import { Searchbar } from "../components/Searchbar";
-import { Questions } from "../data/Data";
 import type { Question } from "../models/Question";
+// import { Questions } from "../data/Data";
+
+async function getQuestions(): Promise<Question[]> {
+  const response = await fetch('http://localhost:8001/api/v1/questions', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  });
+
+  const data = await response.json();
+  return data['results'];
+}
 
 export function QuestionsPage() {
-  const questions: Question[] = Questions;
+  // const questions: Question[]: Questions;
+  const [questions, setQuestions] = useState<Question[]>([]);
+
+  useEffect(() => {
+    getQuestions()
+      .then((data: Question[]) => {
+        setQuestions(data);
+      })
+  }, []);
 
   return (
     <div className="grid grid-cols-5 gap-18 p-10 bg-gray-100">
