34fb48e048143dd01a8b2228dfbb6c7e6ad4fc61
sara lee <saragatinha@gmail.com>
2026-01-28 18:31:17 -0300

    searchbar and paginationbuttons do NOT stretch when there are no questions

--- src/pages/QuestionsPage.tsx
+++ src/pages/QuestionsPage.tsx
@@ -45,7 +45,7 @@ export function QuestionsPage() {
       setCurrentPage={setCurrentPage}
       />
 
-      <div className="grid col-start-2 col-span-3 gap-15">
+      <div className="grid col-start-2 col-span-3 gap-15 items-start">
         <Searchbar />
 
         <div className="grid gap-7">
