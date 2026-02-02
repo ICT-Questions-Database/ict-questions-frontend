d8899d03433cfcc9e416613332ee6873f384899e
Davi Oliveira <olidavi031130@gmail.com>
2026-02-02 16:09:52 -0300

    feat: add react-router-dom to create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Routes, Route } from "react-router-dom";
 import { QuestionsPage } from "./pages/QuestionsPage";
+import RegisterPage from "./pages/Register/RegisterPage";
 
 function App() {
   return (
-    <QuestionsPage />
+    <Routes>
+      <Route path="/questions" element={<QuestionsPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+    </Routes>
   );
 }
 
