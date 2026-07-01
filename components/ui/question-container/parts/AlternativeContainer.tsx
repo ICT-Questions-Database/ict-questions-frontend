9bad72ef626032c29db05298d1d15cb5adeb5644
Marcello Lee <marcellolee03@gmail.com>
2026-07-01 08:38:01 -0300

    feat(layout.tsx): main container for children components

--- components/ui/question-container/parts/AlternativeContainer.tsx
+++ components/ui/question-container/parts/AlternativeContainer.tsx
@@ -7,7 +7,7 @@ interface AlternativeContainerProps {
 export default function AlternativeContainer(
     { alternative }: AlternativeContainerProps
 ) {
-    const baseClassNames = "border rounded-md p-3 transition-all duration-300 hover:scale-102 "
+    const baseClassNames = "border rounded-md p-3 transition-all duration-300 hover:scale-101 "
     const normalClassNames = baseClassNames + "border-[#F4F4F4] bg-[#F8F9FA]"
     const correctClassNames = baseClassNames + "border-[#22C55E] bg-[#E0FFE7]"
     
