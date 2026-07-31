6d6e1bdbff0c3f976f7328f0472a00da9a9beeae
Marcello Lee <marcellolee03@gmail.com>
2026-07-31 00:13:58 -0300

    feat(FilterBox): added option for all questions

--- components/ui/filter-area/filter-selector/filter-box/FilterBox.tsx
+++ components/ui/filter-area/filter-selector/filter-box/FilterBox.tsx
@@ -6,25 +6,34 @@ import { useQueryParams } from "@/hooks/useQueryParams";
 const TRACKS = ["Cloud", "Computing", "Network"];
 
 export default function FilterBox() {
-  const {queryParams, toggleQueryParam} = useQueryParams();
+  const {setQueryParam, queryParams, toggleQueryParam} = useQueryParams();
   
   return (
     <div className="grid grid-cols-2 gap-7 bg-red-800 px-9 py-7 rounded-3xl">
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-5">
         <h1
           className="text-paper font-serif text-lg font-semibold"
         >
           Trilhas
         </h1>
         
-        {TRACKS.map((track) => (
-          <FilterBoxOption
-            key={track}
-            track={track}
-            checked={queryParams.getAll("track").includes(track)}
-            onChange={() => toggleQueryParam("track", track)}
-          />
-        ))}
+        <FilterBoxOption
+          key={"Todas"}
+          track={"Todas"}
+          checked={queryParams.getAll("track").length === 0}
+          onChange={() => setQueryParam("track", "")}
+        />
+
+        <div className="flex flex-col gap-3">
+          {TRACKS.map((track) => (
+            <FilterBoxOption
+              key={track}
+              track={track}
+              checked={queryParams.getAll("track").includes(track)}
+              onChange={() => toggleQueryParam("track", track)}
+            />
+          ))}
+        </div>
       </div>
       
       <div className="flex flex-col gap-2">
